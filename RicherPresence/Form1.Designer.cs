6c8cde7a7b1d247f0f47f0d6a8b8f86ff18dfdc0
JoseBritto <64070234+JoseBritto@users.noreply.github.com>
2021-09-08 12:50:03 +0530

    Add project files.

---
+++ RicherPresence/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿
+namespace RicherPresence
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Details = new System.Windows.Forms.Label();
+            this.DetailsTextBox = new System.Windows.Forms.TextBox();
+            this.SmallText = new System.Windows.Forms.Label();
+            this.SmallTextTextBox = new System.Windows.Forms.TextBox();
+            this.SmallAssetTextBox = new System.Windows.Forms.TextBox();
+            this.SmallAsset = new System.Windows.Forms.Label();
+            this.StateTextBox = new System.Windows.Forms.TextBox();
+            this.State = new System.Windows.Forms.Label();
+            this.LargeAssetTextBox = new System.Windows.Forms.TextBox();
+            this.LargeAsset = new System.Windows.Forms.Label();
+            this.LargeTextTextBox = new System.Windows.Forms.TextBox();
+            this.LargeText = new System.Windows.Forms.Label();
+            this.ClientIdTextBox = new System.Windows.Forms.TextBox();
+            this.ClientId = new System.Windows.Forms.Label();
+            this.UpdateBtn = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Button1Box = new System.Windows.Forms.TextBox();
+            this.Button1LinkBox = new System.Windows.Forms.TextBox();
+            this.Button2LinkBox = new System.Windows.Forms.TextBox();
+            this.Button2Box = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SaveBtn = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.OpenDiag = new System.Windows.Forms.OpenFileDialog();
+            this.SaveDiag = new System.Windows.Forms.SaveFileDialog();
+            this.NotifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.TrayContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // Details
+            // 
+            this.Details.AutoSize = true;
+            this.Details.Location = new System.Drawing.Point(5, 122);
+            this.Details.Name = "Details";
+            this.Details.Size = new System.Drawing.Size(55, 20);
+            this.Details.TabIndex = 0;
+            this.Details.Text = "Details";
+            // 
+            // DetailsTextBox
+            // 
+            this.DetailsTextBox.Location = new System.Drawing.Point(66, 119);
+            this.DetailsTextBox.Name = "DetailsTextBox";
+            this.DetailsTextBox.Size = new System.Drawing.Size(299, 27);
+            this.DetailsTextBox.TabIndex = 1;
+            // 
+            // SmallText
+            // 
+            this.SmallText.AutoSize = true;
+            this.SmallText.Location = new System.Drawing.Point(395, 70);
+            this.SmallText.Name = "SmallText";
+            this.SmallText.Size = new System.Drawing.Size(77, 20);
+            this.SmallText.TabIndex = 2;
+            this.SmallText.Text = "Small Text";
+            // 
+            // SmallTextTextBox
+            // 
+            this.SmallTextTextBox.Location = new System.Drawing.Point(479, 68);
+            this.SmallTextTextBox.Name = "SmallTextTextBox";
+            this.SmallTextTextBox.Size = new System.Drawing.Size(363, 27);
+            this.SmallTextTextBox.TabIndex = 3;
+            // 
+            // SmallAssetTextBox
+            // 
+            this.SmallAssetTextBox.Location = new System.Drawing.Point(479, 122);
+            this.SmallAssetTextBox.Name = "SmallAssetTextBox";
+            this.SmallAssetTextBox.Size = new System.Drawing.Size(363, 27);
+            this.SmallAssetTextBox.TabIndex = 7;
+            // 
+            // SmallAsset
+            // 
+            this.SmallAsset.AutoSize = true;
+            this.SmallAsset.Location = new System.Drawing.Point(395, 124);
+            this.SmallAsset.Name = "SmallAsset";
+            this.SmallAsset.Size = new System.Drawing.Size(85, 20);
+            this.SmallAsset.TabIndex = 6;
+            this.SmallAsset.Text = "Small Asset";
+            // 
+            // StateTextBox
+            // 
+            this.StateTextBox.Location = new System.Drawing.Point(66, 173);
+            this.StateTextBox.Name = "StateTextBox";
+            this.StateTextBox.Size = new System.Drawing.Size(299, 27);
+            this.StateTextBox.TabIndex = 5;
+            // 
+            // State
+            // 
+            this.State.AutoSize = true;
+            this.State.Location = new System.Drawing.Point(5, 176);
+            this.State.Name = "State";
+            this.State.Size = new System.Drawing.Size(43, 20);
+            this.State.TabIndex = 4;
+            this.State.Text = "State";
+            // 
+            // LargeAssetTextBox
+            // 
+            this.LargeAssetTextBox.Location = new System.Drawing.Point(479, 232);
+            this.LargeAssetTextBox.Name = "LargeAssetTextBox";
+            this.LargeAssetTextBox.Size = new System.Drawing.Size(363, 27);
+            this.LargeAssetTextBox.TabIndex = 15;
+            // 
+            // LargeAsset
+            // 
+            this.LargeAsset.AutoSize = true;
+            this.LargeAsset.Location = new System.Drawing.Point(395, 234);
+            this.LargeAsset.Name = "LargeAsset";
+            this.LargeAsset.Size = new System.Drawing.Size(85, 20);
+            this.LargeAsset.TabIndex = 14;
+            this.LargeAsset.Text = "Large Asset";
+            // 
+            // LargeTextTextBox
+            // 
+            this.LargeTextTextBox.Location = new System.Drawing.Point(479, 178);
+            this.LargeTextTextBox.Name = "LargeTextTextBox";
+            this.LargeTextTextBox.Size = new System.Drawing.Size(363, 27);
+            this.LargeTextTextBox.TabIndex = 11;
+            // 
+            // LargeText
+            // 
+            this.LargeText.AutoSize = true;
+            this.LargeText.Location = new System.Drawing.Point(395, 180);
+            this.LargeText.Name = "LargeText";
+            this.LargeText.Size = new System.Drawing.Size(77, 20);
+            this.LargeText.TabIndex = 10;
+            this.LargeText.Text = "Large Text";
+            // 
+            // ClientIdTextBox
+            // 
+            this.ClientIdTextBox.Location = new System.Drawing.Point(66, 70);
+            this.ClientIdTextBox.Name = "ClientIdTextBox";
+            this.ClientIdTextBox.Size = new System.Drawing.Size(299, 27);
+            this.ClientIdTextBox.TabIndex = 17;
+            // 
+            // ClientId
+            // 
+            this.ClientId.AutoSize = true;
+            this.ClientId.Location = new System.Drawing.Point(5, 73);
+            this.ClientId.Name = "ClientId";
+            this.ClientId.Size = new System.Drawing.Size(64, 20);
+            this.ClientId.TabIndex = 16;
+            this.ClientId.Text = "Client Id";
+            // 
+            // UpdateBtn
+            // 
+            this.UpdateBtn.Location = new System.Drawing.Point(632, 335);
+            this.UpdateBtn.Name = "UpdateBtn";
+            this.UpdateBtn.Size = new System.Drawing.Size(155, 59);
+            this.UpdateBtn.TabIndex = 18;
+            this.UpdateBtn.Text = "Update Presence";
+            this.UpdateBtn.UseVisualStyleBackColor = true;
+            this.UpdateBtn.Click += new System.EventHandler(this.UpdateBtn_Click);
+            // 
+            // Output
+            // 
+            this.Output.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Output.Location = new System.Drawing.Point(14, 473);
+            this.Output.Multiline = true;
+            this.Output.Name = "Output";
+            this.Output.ReadOnly = true;
+            this.Output.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Output.Size = new System.Drawing.Size(830, 109);
+            this.Output.TabIndex = 19;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(199, 234);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 25);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Buttons";
+            // 
+            // Button1Box
+            // 
+            this.Button1Box.Location = new System.Drawing.Point(12, 320);
+            this.Button1Box.Name = "Button1Box";
+            this.Button1Box.Size = new System.Drawing.Size(221, 27);
+            this.Button1Box.TabIndex = 25;
+            // 
+            // Button1LinkBox
+            // 
+            this.Button1LinkBox.Location = new System.Drawing.Point(239, 320);
+            this.Button1LinkBox.Name = "Button1LinkBox";
+            this.Button1LinkBox.Size = new System.Drawing.Size(233, 27);
+            this.Button1LinkBox.TabIndex = 26;
+            // 
+            // Button2LinkBox
+            // 
+            this.Button2LinkBox.Location = new System.Drawing.Point(239, 378);
+            this.Button2LinkBox.Name = "Button2LinkBox";
+            this.Button2LinkBox.Size = new System.Drawing.Size(233, 27);
+            this.Button2LinkBox.TabIndex = 28;
+            // 
+            // Button2Box
+            // 
+            this.Button2Box.Location = new System.Drawing.Point(12, 378);
+            this.Button2Box.Name = "Button2Box";
+            this.Button2Box.Size = new System.Drawing.Size(221, 27);
+            this.Button2Box.TabIndex = 27;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(109, 286);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 20);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(330, 286);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 20);
+            this.label4.TabIndex = 30;
+            this.label4.Text = "URL";
+            // 
+            // SaveBtn
+            // 
+            this.SaveBtn.Location = new System.Drawing.Point(14, 431);
+            this.SaveBtn.Name = "SaveBtn";
+            this.SaveBtn.Size = new System.Drawing.Size(94, 29);
+            this.SaveBtn.TabIndex = 31;
+            this.SaveBtn.Text = "Save";
+            this.SaveBtn.UseVisualStyleBackColor = true;
+            this.SaveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(114, 431);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 32;
+            this.button2.Text = "Load";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // OpenDiag
+            // 
+            this.OpenDiag.FileName = "OpenDiag";
+            // 
+            // NotifyIcon
+            // 
+            this.NotifyIcon.ContextMenuStrip = this.TrayContextMenu;
+            this.NotifyIcon.Text = "NotifyIcon";
+            this.NotifyIcon.Visible = true;
+            // 
+            // TrayContextMenu
+            // 
+            this.TrayContextMenu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.TrayContextMenu.Name = "TrayContextMenu";
+            this.TrayContextMenu.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 594);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.SaveBtn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Button2LinkBox);
+            this.Controls.Add(this.Button2Box);
+            this.Controls.Add(this.Button1LinkBox);
+            this.Controls.Add(this.Button1Box);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.UpdateBtn);
+            this.Controls.Add(this.ClientIdTextBox);
+            this.Controls.Add(this.ClientId);
+            this.Controls.Add(this.LargeAssetTextBox);
+            this.Controls.Add(this.LargeAsset);
+            this.Controls.Add(this.LargeTextTextBox);
+            this.Controls.Add(this.LargeText);
+            this.Controls.Add(this.SmallAssetTextBox);
+            this.Controls.Add(this.SmallAsset);
+            this.Controls.Add(this.StateTextBox);
+            this.Controls.Add(this.State);
+            this.Controls.Add(this.SmallTextTextBox);
+            this.Controls.Add(this.SmallText);
+            this.Controls.Add(this.DetailsTextBox);
+            this.Controls.Add(this.Details);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "RicherPresence";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Details;
+        private System.Windows.Forms.TextBox DetailsTextBox;
+        private System.Windows.Forms.Label SmallText;
+        private System.Windows.Forms.TextBox SmallTextTextBox;
+        private System.Windows.Forms.TextBox SmallAssetTextBox;
+        private System.Windows.Forms.Label SmallAsset;
+        private System.Windows.Forms.TextBox StateTextBox;
+        private System.Windows.Forms.Label State;
+        private System.Windows.Forms.TextBox LargeAssetTextBox;
+        private System.Windows.Forms.Label LargeAsset;
+        private System.Windows.Forms.TextBox LargeTextTextBox;
+        private System.Windows.Forms.Label LargeText;
+        private System.Windows.Forms.TextBox ClientIdTextBox;
+        private System.Windows.Forms.Label ClientId;
+        private System.Windows.Forms.Button UpdateBtn;
+        private System.Windows.Forms.TextBox Output;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Button1Box;
+        private System.Windows.Forms.TextBox Button1LinkBox;
+        private System.Windows.Forms.TextBox Button2LinkBox;
+        private System.Windows.Forms.TextBox Button2Box;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button SaveBtn;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog OpenDiag;
+        private System.Windows.Forms.SaveFileDialog SaveDiag;
+        private System.Windows.Forms.NotifyIcon NotifyIcon;
+        private System.Windows.Forms.ContextMenuStrip TrayContextMenu;
+    }
+}
+
---
+++ RicherPresence/Form1.cs
@@ -0,0 +1,330 @@
+﻿using DiscordRPC;
+using DiscordRPC.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RicherPresence
+{
+    public partial class Form1 : Form
+    {
+        DiscordRpcClient client;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Log(string s)
+        {
+            Output.Text = s + "\r\n" + Output.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OpenDiag.FileOk += OpenDiag_FileOk;
+            OpenDiag.Filter = "Json Files|*.json";
+            SaveDiag.FileOk += SaveDiag_FileOk;
+            SaveDiag.DefaultExt = "json";
+            SaveDiag.Filter = "Json Files|*.json";
+            SaveDiag.AddExtension = true;
+
+            
+            try
+            {
+                if (!File.Exists("data.json"))
+                    return;
+
+                var json = File.ReadAllText("data.json");
+
+                var save = JsonConvert.DeserializeObject<SaveData>(json);
+
+                ClientIdTextBox.Text = save.cid;
+
+                DetailsTextBox.Text = save.RichPresence.Details;
+
+                StateTextBox.Text = save.RichPresence.State;
+
+                SmallTextTextBox.Text = save.RichPresence.Assets.SmallImageText;
+                SmallAssetTextBox.Text = save.RichPresence.Assets.SmallImageKey;
+
+                LargeTextTextBox.Text = save.RichPresence.Assets.LargeImageText;
+                LargeAssetTextBox.Text = save.RichPresence.Assets.LargeImageKey;
+
+                // PartySizeBox.Text = save.RichPresence.Party?.Size.ToString();
+                // PartyMaxBox.Text = save.RichPresence.Party?.Max.ToString();
+
+                if (!save.RichPresence.HasButtons())
+                    return;
+
+                if (save.RichPresence.Buttons.Length == 0)
+                    return;
+
+                Button1Box.Text = save.RichPresence.Buttons[0].Label;
+                Button1LinkBox.Text = save.RichPresence.Buttons[0].Url;
+
+                if (save.RichPresence.Buttons.Length == 1)
+                    return;
+                Button2Box.Text = save.RichPresence.Buttons[1].Label;
+                Button2LinkBox.Text = save.RichPresence.Buttons[1].Url;
+
+
+            }
+            catch (Exception ex)
+            {
+                Log($"ERROR: {ex.Message}");
+            }
+        }
+
+        private void UpdateBtn_Click(object sender, EventArgs e)
+        {   
+            var presence = GetPresence();
+            
+            if (presence == null)
+                return;
+
+            client.SetPresence(presence);
+        }
+
+        private RichPresence GetPresence()
+        {
+            if (!long.TryParse(ClientIdTextBox.Text.Trim(), out var cid))
+            {
+                Log("ERROR: Provide a valid client id!");
+                return null;
+            }
+
+            if (client == null)
+            {
+                client = new DiscordRpcClient(cid.ToString());
+
+
+                client.OnPresenceUpdate += (sender, e) =>
+                {
+                    Output.Invoke((MethodInvoker)delegate {
+                        var json = JsonConvert.SerializeObject(e.Presence);
+                        // Running on the UI thread
+                        Log($"Received Update! {json}");
+                    });
+
+                };
+
+                client.Initialize();
+            }
+
+            var presence = new RichPresence
+            {
+                Assets = new Assets
+                {
+                    LargeImageKey = LargeAssetTextBox.Text,
+                    LargeImageText = LargeTextTextBox.Text,
+                    SmallImageKey = SmallAssetTextBox.Text,
+                    SmallImageText = SmallTextTextBox.Text
+                },
+
+                Details = DetailsTextBox.Text,
+                State = StateTextBox.Text,
+                Buttons = GetButtons(),
+                Party = GetParty(),
+
+            };
+
+            return presence;
+        }
+
+        private Party GetParty()
+        {
+            return null;
+           /* var party = new Party();
+
+            if (string.IsNullOrWhiteSpace(PartySizeBox.Text))
+                return null;
+
+            if (!int.TryParse(PartySizeBox.Text, out int size))
+                return null;
+
+            if (!int.TryParse(PartyMaxBox.Text, out int max))
+                max = size;
+            
+            party.Size = size;
+            party.Max = max;
+
+            return party;*/
+        }
+
+        private DiscordRPC.Button[] GetButtons()
+        {
+            string text1 = Button1Box.Text.Trim();
+            string link1 = Button1LinkBox.Text?.Trim();
+
+            if (!string.IsNullOrWhiteSpace(text1))
+            {
+                if (link1 != null && !Uri.IsWellFormedUriString(link1, UriKind.Absolute))
+                {
+                    Log("ERROR: Link for button 1 is not valid");
+                    return null;
+                }
+            }
+
+            string text2 = Button2Box.Text;
+            string link2 = Button2LinkBox.Text?.Trim();
+
+            if (!string.IsNullOrWhiteSpace(text2))
+            {
+                if (link2 != null && !Uri.IsWellFormedUriString(link2, UriKind.Absolute))
+                {
+                    Log("ERROR: Link for button 2 is not valid");
+                    return null;
+                }
+            }
+            var list = new List<DiscordRPC.Button>();
+
+            if (!string.IsNullOrWhiteSpace(text1))
+            {
+                list.Add(new DiscordRPC.Button { Label = text1, Url = link1 });
+            }
+
+            if (!string.IsNullOrWhiteSpace(text2))
+            {
+                list.Add(new DiscordRPC.Button { Label = text2, Url = link2 });
+            }
+
+            if (list.Count > 0)
+                return list.ToArray();
+
+            return null;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var res = OpenDiag.ShowDialog();
+            
+        }
+
+        private void OpenDiag_FileOk(object sender, CancelEventArgs e)
+        {
+            Log(OpenDiag.FileName);
+            if (string.IsNullOrWhiteSpace(OpenDiag.FileName))
+                return;
+
+            var json = File.ReadAllText(OpenDiag.FileName);
+
+            try
+            {
+                var save = JsonConvert.DeserializeObject<SaveData>(json);
+
+                ClientIdTextBox.Text = save.cid;
+
+                DetailsTextBox.Text = save.RichPresence.Details;
+
+                StateTextBox.Text = save.RichPresence.State;
+
+                SmallTextTextBox.Text = save.RichPresence.Assets.SmallImageText;
+                SmallAssetTextBox.Text = save.RichPresence.Assets.SmallImageKey;
+
+                LargeTextTextBox.Text = save.RichPresence.Assets.LargeImageText;
+                LargeAssetTextBox.Text = save.RichPresence.Assets.LargeImageKey;
+
+               // PartySizeBox.Text = save.RichPresence.Party?.Size.ToString();
+               // PartyMaxBox.Text = save.RichPresence.Party?.Max.ToString();
+
+                if (!save.RichPresence.HasButtons())
+                    return;
+
+                if (save.RichPresence.Buttons.Length == 0)
+                    return;
+
+                Button1Box.Text = save.RichPresence.Buttons[0].Label;
+                Button1LinkBox.Text = save.RichPresence.Buttons[0].Url;
+
+                if (save.RichPresence.Buttons.Length == 1)
+                    return;
+                Button2Box.Text = save.RichPresence.Buttons[1].Label;
+                Button2LinkBox.Text = save.RichPresence.Buttons[1].Url;
+
+
+            }
+            catch (Exception ex)
+            {
+                Log($"ERROR: {ex.Message}");
+            }
+
+
+        }
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            var result = SaveDiag.ShowDialog();
+        }
+
+        private void SaveDiag_FileOk(object sender, CancelEventArgs e)
+        {
+
+            var presence = GetPresence();
+
+            if (presence == null)
+            {
+                Log("ERROR: Save Failed!");
+                return;
+            }
+            var save = new SaveData { cid = ClientIdTextBox.Text, RichPresence = presence };
+
+            var json = JsonConvert.SerializeObject(save, Formatting.Indented);
+
+            if (string.IsNullOrWhiteSpace(SaveDiag.FileName))
+                return;
+
+            File.WriteAllText(SaveDiag.FileName, json);
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+                var presence = GetPresence();
+
+                if (presence == null)
+                {
+                    Dispose();
+                    return;
+                }
+                var save = new SaveData { cid = ClientIdTextBox.Text, RichPresence = presence };
+
+                var json = JsonConvert.SerializeObject(save, Formatting.Indented);
+
+                
+                File.WriteAllText("data.json", json);
+
+            }
+            catch { }
+
+            void Dispose()
+            {
+                if (client != null)
+                    client.Dispose();
+
+                client = null;
+            }
+           
+        }
+    }
+
+    public class SaveData
+    {
+        public string cid;
+
+        public RichPresence RichPresence;
+    }
+}
